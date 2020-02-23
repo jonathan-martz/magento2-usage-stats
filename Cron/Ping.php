38aa6bdfa7bc8074efc182e6d534315fc4db13bc
jonathan-martz <github@jmartz.de>
2020-02-23 13:45:19 +0100

    add logic for module collection

--- Cron/Ping.php
+++ Cron/Ping.php
@@ -3,6 +3,7 @@
 namespace JonathanMartz\UsageStats\Cron;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Module\FullModuleList;
 
 /**
  * Class Ping
@@ -11,44 +12,77 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 class Ping
 {
     const ENABLE = 'useagestats/general/enable';
-    const NAMES = 'useagestats/general/names';
+    const NAME = 'useagestats/general/name';
     const ENDPOINT = 'useagestats/general/endpoint';
 
     private $scopeConfig;
 
-    public function __construct(ScopeConfigInterface $scopeConfig)
+    private $moduleList;
+
+    public function __construct(ScopeConfigInterface $scopeConfig, FullModuleList $moduleList)
     {
         $this->scopeConfig = $scopeConfig;
+        $this->moduleList = $moduleList;
     }
 
-    public function getNamespaces(): array
+    public function getNamespace()
     {
-        return explode(',', $this->scopeConfig->getValue(self::NAMES));
+        return (string)$this->scopeConfig->getValue(self::NAME);
     }
 
-    public function getEndpoint(): string
+    public function getEndpoint()
     {
         return (string)$this->scopeConfig->getValue(self::ENDPOINT);
     }
 
-    public function isActive(): bool
+    public function isActive()
     {
         return (bool)$this->scopeConfig->getValue(self::ENABLE);
     }
 
-    public function getModuleList(): array
+    public function startsWith($string, $startString)
     {
-        return [];
+        $len = strlen($startString);
+        return (substr($string, 0, $len) === $startString);
     }
 
-    public function ping(): void
+    public function collectModules()
+    {
+        $modules = $this->moduleList->getAll();
+        $tmp = [];
+
+        foreach($modules as $key => $value) {
+            if($this->startsWith($key, 'JonathanMartz')) {
+                var_dump($key);
+
+                // version
+                $tmp[] = $key;
+
+            }
+        }
+
+
+        return $tmp;
+    }
+
+    public function getModuleList()
+    {
+        $modules = $this->collectModules();
+
+        return $modules;
+    }
+
+    public function ping()
     {
         $ch = curl_init();
+        $json = json_encode($this->getModuleList(), JSON_FORCE_OBJECT);
         curl_setopt($ch, CURLOPT_URL, $this->getEndpoint());
         curl_setopt($ch, CURLOPT_USERAGENT, 'Its me Mario ;)');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->getModuleList()));
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        $output = curl_exec($ch);
+        var_dump($output);
+        var_dump($json);
         curl_close($ch);
     }
 
--- README.md
+++ README.md
@@ -3,8 +3,3 @@ Note: Still in development!!!
 
 #### Features:
 * monitor module usage
-
-#### Todo:
-* add logic to handle multiple namespaces
-* add logic to handle endpoint for every namespace
-* multi install ?
